222a79bc7f1a040288757ccfcdb065e5030c2794
Deniz Acay <deniz@botanalytics.co>
2023-02-21 11:40:54 +0300

    Add health check endpoint

--- server.js
+++ server.js
@@ -4,4 +4,14 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+
+    // JSON cevap icin:
+    res.json({
+        healthy: true
+    })
+    // Duz cevap icin (icerigin tipine gore Content-Type degisiyor):
+    // res.send("OK");
+})
+
 module.exports = app; // export default app; <-- ES6 syntax
