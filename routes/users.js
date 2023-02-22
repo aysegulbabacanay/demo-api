26057de03b9be8cc45cff27a51561e94ee6d8bab
Deniz Acay <deniz@botanalytics.co>
2023-02-22 11:01:10 +0300

    Add POST /users/ endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,52 @@
 const express = require('express');
+const uuid = require('uuid');
+
 const router = express.Router();
+
+const db = require('../dbConfig');
 
 /**
  * GET /users/
  */
 router.get('/', async (req, res) => {
 
+
+    // db.from('users').select('*')
 });
+
+/**
+ * POST /users/
+ */
+router.post('/', async (req, res) => {
+
+    let { name, email, password } = req.body;
+
+    let id = uuid.v4().toString();
+
+    let userObj = {
+        name,
+        email,
+        password,
+        id
+    }
+
+    try {
+
+        await db('users').insert([userObj]);
+
+        res.json({
+            success: true
+        })
+
+    } catch(err) {
+
+        console.error(err);
+
+        res.json({
+            success: false,
+            error: err
+        })
+    }
+})
 
 module.exports = router;
